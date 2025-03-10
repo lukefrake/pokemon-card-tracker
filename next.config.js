a7bf08a935db2310192755f09bb423850aa7f01c
Luke Frake <lfrake@spotify.com>
2025-03-10 21:41:52 +0000

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/pokemon-card-tracker' : '';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/pokemon-card-tracker',
+  basePath: basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
